1bc0020260c803ddac2596c5bab5ffd632ab8c05
stmonitoring2 <stevemonitoring.gathirus@gmail.com>
2025-09-07 17:26:53 -0500

    Delete app/app directory

--- app/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-import "./globals.css";
-export const metadata = { title: "KE Top 500 – Podcasts & Interviews" };
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
