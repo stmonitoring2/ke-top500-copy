812acd80326f50d42e276d6a59cf343cba29956d
stmonitoring2 <stevemonitoring.gathirus@gmail.com>
2025-10-11 19:23:38 -0500

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-export default {
+// CommonJS export expected by Next (since your package.json has no "type": "module")
+module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+    autoprefixer: {}
+  }
+};
