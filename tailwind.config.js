83348d7f38625fedf20c119d53ecd2bf7ec0e034
stmonitoring2 <stevemonitoring.gathirus@gmail.com>
2025-10-11 19:24:25 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: { extend: {} },
-  plugins: [],
-}
+  plugins: []
+};
