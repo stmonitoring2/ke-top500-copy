3bfb89914f53a3a4af98c421eff8fe4ff90b1e32
stmonitoring2 <stevemonitoring.gathirus@gmail.com>
2025-10-17 18:31:48 -0500

    Update HeaderAuth.tsx

--- components/HeaderAuth.tsx
+++ components/HeaderAuth.tsx
@@ -1,11 +1,8 @@
-// components/HeaderAuth.tsx
 import HeaderAuthButtons from "./HeaderAuthButtons";
 import { createClient } from "@/lib/supabase-server";
 
 export default async function HeaderAuth() {
   const supabase = createClient();
   const { data: { session } } = await supabase.auth.getSession();
-  const isAuthed = !!session?.user?.id;
-
-  return <HeaderAuthButtons isAuthed={isAuthed} />;
+  return <HeaderAuthButtons isAuthed={!!session?.user?.id} />;
 }
