988532de23815cd07218f7b336b9bc8cc8183794
stmonitoring2 <stevemonitoring.gathirus@gmail.com>
2025-10-20 21:01:25 -0500

    Update supabase-browser.ts

--- lib/supabase-browser.ts
+++ lib/supabase-browser.ts
@@ -1,25 +1,9 @@
-// lib/supabase-server.ts
-import { cookies } from "next/headers";
-import { createServerClient } from "@supabase/ssr";
+// lib/supabase-browser.ts
+import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
-  const cookieStore = cookies();
-
-  return createServerClient(
+  return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value;
-        },
-        set(name: string, value: string, options: any) {
-          cookieStore.set({ name, value, ...options });
-        },
-        remove(name: string, options: any) {
-          cookieStore.set({ name, value: "", ...options });
-        },
-      },
-    }
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 }
