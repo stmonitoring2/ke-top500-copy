b778d63bd3b44367427e901b988fb17d8251ce07
stmonitoring2 <stevemonitoring.gathirus@gmail.com>
2025-10-23 19:01:21 -0500

    Refactor createClient to supabaseBrowser arrow function

--- lib/supabase-browser.ts
+++ lib/supabase-browser.ts
@@ -1,9 +1,8 @@
 // lib/supabase-browser.ts
 import { createBrowserClient } from "@supabase/ssr";
 
-export function createClient() {
-  return createBrowserClient(
+export const supabaseBrowser = () =>
+  createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
-}
